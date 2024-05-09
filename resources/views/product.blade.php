3e8c9a91b6a955f9c90bff44ac02b76585e0f344
NicolaBellini <bellininicola4366@gmail.com>
2024-05-09 15:02:31 +0200

    [fix]badges position and liked badges

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -20,21 +20,21 @@
                             class="hovered-image"
                         />
 
-                    </div>
-
-                    <div class="tag">
-                    <!-- applico la classe sostenibilità solo se il .value è uguale a sostenibilità -->
-                        @foreach($product['badges'] as $badge)
-                            <div class="badge {{ $badge['value'] === 'Sostenibilità' ? 'sost' : 'discount' }}">
-                                {{ $badge['value'] }}
+                        <div class="tag">
+                        <!-- applico la classe sostenibilità solo se il .value è uguale a sostenibilità -->
+                            @foreach($product['badges'] as $badge)
+                                <div class="badge {{ $badge['value'] === 'Sostenibilità' ? 'sost' : 'discount' }}">
+                                    {{ $badge['value'] }}
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="liked"  onclick="$set($product['isInFavorites'], !$product['isInFavorites'])"  >
+                            <div>
+                                <span><i class="fa-solid fa-heart {{ $product['isInFavorites'] ? 'favorite' : '' }}"></i></span>
                             </div>
-                        @endforeach
-                    </div>
-                    <div class="liked"  onclick="$set($product['isInFavorites'], !$product['isInFavorites'])"  >
-                        <div>
-                            <span><i class="fa-solid fa-heart {{ $product['isInFavorites'] ? 'favorite' : '' }}"></i></span>
                         </div>
                     </div>
+
                     <div class="info">
                         <div class="brand">{{ $product['brand'] }}</div>
                         <div>
